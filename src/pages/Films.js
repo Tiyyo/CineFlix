02d7c3e6f169e98ca156388b1c2035224fa69c77
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2023-01-04 18:22:57 +0100

    Create Films.js

---
+++ src/pages/Films.js
@@ -0,0 +1,32 @@
+<>
+  <div className='header'
+    <Navigation></Navigation>
+    <SearchBar/>
+  </div>
+  { searchIsActive() ? (<div className="search--result"><MovieCard/></div>) :
+  (
+  { 
+   loading ? <Loader /> : (                    
+   <div classname='main--content'>
+    <Trendings>
+      <BannerCard>
+    <Trendings>
+    <Lastreleas
+    <Recomandations>
+        <MovieCard/>
+    </Recomandations>
+    <Genre />
+    <Genre />
+    <Genre />                                         
+   </div>)
+   )                  
+   }                    
+   }
+</>
+    
+    
+    
+    
+  
+  
+  
