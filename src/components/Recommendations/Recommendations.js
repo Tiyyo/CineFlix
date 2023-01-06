48e0c909e4c724f8ec4c7a7dce40bd705cc14123
Steeve Matou <steeve.matou@gmail.com>
2023-01-06 09:08:38 +0100

    data

--- src/components/Recommendations/Recommendations.js
+++ src/components/Recommendations/Recommendations.js
@@ -10,34 +10,16 @@ import "swiper/css/pagination";
 import MovieCard from "../moviecard/MovieCard";
 
 const Recommendations = () => {
-  const constructionMovies = ["Allo", "Hexu", 4, 5];
+  const constructionMovies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
 
   return (
     <div className="recommendation--list">
       <h2>Recommend for you</h2>
-      <Swiper
-        slidesPerView={2}
-        grid={{
-          rows: 2,
-          colums: 2,
-        }}
-        spaceBetween={20}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Grid, Pagination]}
-        className="recommendation--swiper"
-      >
+      <div className="recommendation--swiper">
         {constructionMovies.map((movie) => {
-          return (
-            <div>
-              <SwiperSlide>{movie}</SwiperSlide>
-              <SwiperSlide>{movie}</SwiperSlide>;
-              <SwiperSlide>{movie}</SwiperSlide>;
-            </div>
-          );
+          return <div className="swiper-slide">Film {movie}</div>;
         })}
-      </Swiper>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,35 +10,141 @@ import Recommendations from "../../components/Recommendations/Recommendations";
 import Avatar from "../../components/Navigation/Avatar";
 
 const Home = () => {
+  let currentDate = new Date();
+  const date = currentDate.setMonth(-1);
+
   const URLTopRated =
     "https://api.themoviedb.org/3/discover/movie?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
 
+  const trendingAllUrl =
+    "https://api.themoviedb.org/3/trending/all/week?api_key=3e2abd7e10753ed410ed7439f7e1f93f";
+
+  const lastReleaseMoviesUrl = `https://api.themoviedb.org/3/discover/movie?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=release_date.desc&include_adult=false&include_video=true&page=1&release_date.lte=${date}&watch_region=FR&with_watch_monetization_types=flatrate`;
+
+  const lastReleaseTvShowUrl = `https://api.themoviedb.org/3/discover/tv?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=popularity.asc&air_date.lte=${date}&page=1&timezone=Europe%2FParis&include_null_first_air_dates=false&with_watch_providers=FR&with_watch_monetization_types=flatrate&with_status=0&with_type=0`;
+
+  const recommendationsMoviesUrl =
+    "https://api.themoviedb.org/3/discover/movie?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&vote_count.gte=5000&vote_average.gte=8&with_watch_monetization_types=flatrate";
+
+  const recommendationsTvShowUrl =
+    "https://api.themoviedb.org/3/discover/tv?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=vote_average.desc&page=1&vote_average.gte=6&vote_count.gte=100&include_null_first_air_dates=false&with_watch_providers=FR&with_watch_monetization_types=flatrate&with_status=0&with_type=0";
+
+  const urls = [
+    trendingAllUrl,
+    lastReleaseMoviesUrl,
+    lastReleaseTvShowUrl,
+    recommendationsMoviesUrl,
+    recommendationsTvShowUrl,
+  ];
+
   const [searchIsActive, setSearchActive] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [trendingAll, setTrendingAll] = useState([]);
+  const [lastReleaseMovies, setLastReleaseMovies] = useState([]);
+  const [lastReleaseTvShow, setLastReleaseTvShow] = useState([]);
+  const [recommendationsMovie, setRecommendationsMovie] = useState([]);
+  const [recommendationsTvShow, setRecommendationsTvShow] = useState([]);
+
   const [topMovies, setTopMovies] = useState(null);
   const [lastestMovies, setlastestMovies] = useState([]);
   const [genreList, setGenreList] = useState([]);
-  const [loading, setLoading] = useState(false);
+
+  const token = {
+    headers: {
+      Authorizarion:
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTJhYmQ3ZTEwNzUzZWQ0MTBlZDc0MzlmN2UxZjkzZiIsInN1YiI6IjYzYWNhZjI2YmU0YjM2MDA4YTZjNzFlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BMRrpkyDtkLPeFuYLeahtwVp_wU8c9Xo4ynqvH-KJhk",
+      ContentType: "application/json;charset=utf-8",
+    },
+  };
+
+  const lastReleaseAll = [...lastReleaseMovies, ...lastReleaseTvShow];
+  const recommendationsAll = [
+    ...recommendationsMoviesUrl,
+    ...recommendationsTvShow,
+  ];
 
   useEffect(() => {
-    const fetchGenreList = async () => {
-      const result = await axios
-        .get(
-          "https://api.themoviedb.org/3/genre/movie/list?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR"
-        )
-        .then((res) => console.log(res.data.genres));
-    };
-    fetchGenreList();
-  }, []);
-
-  useEffect(() => {
-    const fetchDataTopMovies = async () => {
-      const result = await axios.get(URLTopRated).then((res) => {
-        setTopMovies(res.data.results);
+    const fetchData = async () => {
+      const result = await axios.get(trendingAllUrl).then((res) => {
+        setTrendingAll(res.data.results);
         setLoading(true);
       });
     };
-    fetchDataTopMovies();
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(lastReleaseMoviesUrl).then((res) => {
+        console.log(res.data.results);
+        setLastReleaseMovies(res.data.results);
+        setLoading(true);
+      });
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(lastReleaseTvShowUrl).then((res) => {
+        console.log(res.data.results);
+        setLastReleaseTvShow(res.data.results);
+        setLoading(true);
+      });
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(recommendationsMoviesUrl).then((res) => {
+        setRecommendationsMovie(res.data.results);
+        setLoading(true);
+      });
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(recommendationsMoviesUrl).then((res) => {
+        setRecommendationsMovie(res.data.results);
+        setLoading(true);
+      });
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get(recommendationsTvShowUrl).then((res) => {
+        setRecommendationsTvShow(res.data.results);
+        setLoading(true);
+      });
+    };
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   const fetchGenreList = async () => {
+  //     const result = await axios
+  //       .get(
+  //         "https://api.themoviedb.org/3/genre/movie/list?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR"
+  //       )
+  //       .then((res) => console.log(res.data.genres));
+  //   };
+  //   fetchGenreList();
+  // }, []);
+
+  // useEffect(() => {
+  //   const fetchDataTopMovies = async () => {
+  //     const result = await axios.get(URLTopRated).then((res) => {
+  //       setTopMovies(res.data.results);
+  //       setLoading(true);
+  //     });
+  //   };
+  //   fetchDataTopMovies();
+  // }, []);
 
   return (
     <div className="app">
@@ -66,3 +172,6 @@ const Home = () => {
 };
 
 export default Home;
+
+// --header 'Authorization: Bearer <<access_token>>' \
+// --header 'Content-Type: application/json;charset=utf-8'
