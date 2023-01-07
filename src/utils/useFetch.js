1ad541ec96e251836f501e61108030511a482500
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2023-01-07 17:24:30 +0100

    Update useFetch.js

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -2,13 +2,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 
-fonction useFetch (url) {
+fonction useFetch (url, setLoading) {
   const [content, setContent] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(url).then((res) => {
-        setContent(res.data.results);
-      }, []);
+        setContent(res.data.results)
+        setLoading(true);
+      }).catch((error) => console.log(error), []);
     };
   });
   return content;
