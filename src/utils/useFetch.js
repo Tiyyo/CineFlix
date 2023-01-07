e4736ad45801008fc159ba5309a8e83ef0936845
Tiyyo <111092370+Tiyyo@users.noreply.github.com>
2023-01-07 16:23:36 +0100

    Update useFetch.js

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const top =
-  "https://api.themoviedb.org/3/discover/movie?api_key=3e2abd7e10753ed410ed7439f7e1f93f&language=fr-FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
 
-const useFetch = (url) => {
+fonction useFetch (url) {
   const [content, setContent] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
@@ -16,4 +14,4 @@ const useFetch = (url) => {
   return content;
 };
 
-useFetch(top);
+export default useFetch;
