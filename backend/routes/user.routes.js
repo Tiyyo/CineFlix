eec864040efb8da6014766a051cc49acd2a9f0b0
Steeve Matou <steeve.matou@gmail.com>
2023-01-31 12:22:44 +0100

    create user database

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { createUser } = require("../controllers/user.controller");
+
+router.post("/", createUser);
+
 module.exports = router;
